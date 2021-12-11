b5f13a4005ee717069f4636e1be3aca684eba5f0
Esron <esron.dtamar@gmail.com>
2021-12-11 16:54:49 -0300

    Adiciona as validações mais básicas

--- src/rooms/create-room.dto.ts
+++ src/rooms/create-room.dto.ts
@@ -1,7 +1,18 @@
+import { IsAlphanumeric, IsArray, IsNumber } from 'class-validator';
+
 export class CreateRoomDTO {
+  @IsNumber()
   rounds: number;
+
+  @IsNumber()
   maxPlayers: number;
+
+  @IsAlphanumeric()
   password: string;
+
+  @IsArray()
   categories: string;
+
+  @IsArray()
   letters: string;
 }
