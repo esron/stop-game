c9a74424b262bb61c09a6e47dd75055399df323b
Esron <esron.dtamar@gmail.com>
2021-12-13 19:11:05 -0300

    Adiciona validações de obrigatoriedade

--- src/rooms/create-room.dto.ts
+++ src/rooms/create-room.dto.ts
@@ -1,18 +1,22 @@
-import { IsAlphanumeric, IsArray, IsNumber } from 'class-validator';
+import { IsAlphanumeric, IsArray, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateRoomDTO {
   @IsNumber()
+  @IsNotEmpty()
   rounds: number;
 
   @IsNumber()
+  @IsNotEmpty()
   maxPlayers: number;
 
   @IsAlphanumeric()
   password: string;
 
   @IsArray()
+  @IsNotEmpty()
   categories: string;
 
   @IsArray()
+  @IsNotEmpty()
   letters: string;
 }
